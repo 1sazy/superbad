dcc698f8f4c26389556918334c5dd10eb4129a01
1sazy <iliyas.isalimovv@gmail.com>
2024-09-20 13:39:01 +0500

    Add files via upload

---
+++ superbad/js/data.js
@@ -0,0 +1,54 @@
+const teamsData = [
+    {
+        id: 1,
+        name: "Супер Звёзды",
+        teamElo: 1600,
+        players: [
+            {
+                login: "ivanpetrov123",
+                name: "Иван Петров",
+                personalElo: 1500
+            },
+            {
+                login: "annasidorova",
+                name: "Анна Сидорова",
+                personalElo: 1600
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Космические Ракеты",
+        teamElo: 1700,
+        players: [
+            {
+                login: "dmitriykuznetsov",
+                name: "Дмитрий Кузнецов",
+                personalElo: 1700
+            },
+            {
+                login: "olgalarina",
+                name: "Ольга Ларина",
+                personalElo: 1800
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Дикие Львы",
+        teamElo: 1450,
+        players: [
+            {
+                login: "sergeivanov",
+                name: "Сергей Иванов",
+                personalElo: 1400
+            },
+            {
+                login: "elenapetrova",
+                name: "Елена Петрова",
+                personalElo: 1500
+            }
+        ]
+    },
+    // Добавьте больше команд по аналогии
+];
